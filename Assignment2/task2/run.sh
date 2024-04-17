07e2afc89f2288ddce516a3880e5af240d890415
David Ahnlund <david.ahnlund@gmail.com>
2024-04-17 14:32:44 +0200

    updated bash script

--- Assignment2/task2/run.sh
+++ Assignment2/task2/run.sh
@@ -1,17 +1,25 @@
-#!/bin/bash 
+#!/bin/bash -l
+# The -l above is required to get the full environment with modules
+
+#SBATCH -J run
+#SBATCH -t 0:05:00
+#SBATCH -A edu24.DD2356
+# Number of nodes
+#SBATCH -p shared
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=128
+#SBATCH --nodes=1
+#SBATCH -e error_file.e
+
 for n in 1 32 64 128;
 do  
     echo Running case: $n threads.
     export OMP_NUM_THREADS=$n
-    cc -O2 -o stream stream.c -fopenmp
-    #clang -O2 -o stream stream.c -fopenmp
+    OMP_PLACES=cores
+    cc -O2 -o stream stream.c -openmp
     for i in $(seq 1 5);
     do
-        ./stream > results.txt
-        python3 record.py
+        srun -n $n ./stream > results.txt
+        srun -n $n python3 record.py
     done
 done
-python3 plot_results.py
-rm avg.csv
-rm results.txt
-rm ./stream
--- Assignment2/task3/run.sh
+++ Assignment2/task3/run.sh
@@ -1,10 +1,22 @@
-#!/bin/bash 
-echo Running serial_sum...
-clang -o serial_sum serial_sum.c -fopenmp #Do not use any optimization here, messes with the timing.
-./serial_sum
-rm serial_sum
+#!/bin/bash -l
+# The -l above is required to get the full environment with modules
 
-echo Running omp_sum...
-clang -o omp_sum omp_sum.c -fopenmp #Do not use any optimization here, messes with the timing.
-./omp_sum
+#SBATCH -J run
+#SBATCH -t 0:05:00
+#SBATCH -A edu24.DD2356
+# Number of nodes
+#SBATCH -p shared
+#SBATCH --ntasks-per-node=32
+#SBATCH --cpus-per-task=1
+#SBATCH --nodes=1
+#SBATCH -e error_file.e
+
+# Run the executable file 
+#echo Running serial_sum...
+#cc -o serial_sum serial_sum.c -openmp #Do not use any optimization here, messes with the timing.
+#srun -n 1 ./serial_sum > serial_sum_output
+#rm serial_sum
+
+cc -o omp_sum omp_sum.c -openmp #Do not use any optimization here, messes with the timing.
+srun -n 32 ./omp_sum > omp_sum_output.txt
 rm omp_sum
