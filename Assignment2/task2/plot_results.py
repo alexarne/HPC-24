c1e72a8cebb364086ce5f47a6715b544c45ba3aa
David <david.ahnlund@gmail.com>
2024-04-17 16:57:59 +0200

    cleanup

--- Assignment2/task2/plot_results.py
+++ Assignment2/task2/plot_results.py
@@ -1,7 +1,18 @@
 from matplotlib import pyplot as plt
 import numpy as np
 
-
+for n in [1,32,64,128]:
+    for i in range(5):
+        with open(f"./results/results_{n}_{i+1}.txt", "r") as file:
+            averages = []
+            for k,l in enumerate(file):
+                if k in (26,27,28,29):
+                    averages.append(l.split()[1])
+        file.close()
+        with open("./avg.csv", "a") as file:
+            file.writelines(",".join(averages))
+            file.writelines("\n")
+            
 data = np.genfromtxt('avg.csv', delimiter=',')
 cases = [1,32,64,128]
 data = np.reshape(data, (-1,5,4))
