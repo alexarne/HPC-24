4d43593df877bd70d7bf0e9988ae8b74053e23e8
David Ahnlund <david.ahnlund@gmail.com>
2024-04-18 10:46:29 +0200

    added data from dardel

--- Assignment2/task2/run.sh
+++ Assignment2/task2/run.sh
@@ -9,6 +9,7 @@
 #SBATCH --cpus-per-task=64
 #SBATCH --nodes=1
 #SBATCH -e error_file.e
+#SBATCH --output=output.log
 
 for n in 1 32 64 128;
 do
@@ -23,4 +24,12 @@ done
 
 export OMP_NUM_THREADS=128
 OMP_PLACES=cores
-
+cc -O2 -o stream stream.c -openmp
+cc -O2 -o stream_static stream_static.c -openmp
+cc -O2 -o stream_dynamic stream_dynamic.c -openmp
+for i in $(seq 1 5);
+do
+    srun -n 1 ./stream > ./results_guided128/result_${i}.txt
+    srun -n 1 ./stream_static > ./results_static128/result_${i}.txt
+    srun -n 1 ./stream_dynamic > ./results_dynamic128/result_${i}.txt
+done
