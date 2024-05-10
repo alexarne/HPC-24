c44ac499131dd37902d07f431259fda624fc9dfe
David Ahnlund <david.ahnlund@gmail.com>
2024-05-10 10:45:12 +0200

    added results from dardel

--- Assignment3/task4/parallel_reduce.c
+++ Assignment3/task4/parallel_reduce.c
@@ -9,7 +9,7 @@
 #define SEED     921
 
 int main(int argc, char* argv[]) {
-    int local_count = 0, flip = 1 << 28;
+    int local_count = 0, flip = 1000000000;
     double x, y, z, pi;
     int rank, num_ranks, i, iter, provided;
     
