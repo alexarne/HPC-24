2d6f9758e0cbf0482f93b529b800859e60e76022
Alex Gunnarsson <alexgu@kth.se>
2024-05-12 04:27:04 +0200

    idk progress

---
+++ Assignment3/bonus/run.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -l
+
+#SBATCH -J run
+#SBATCH -t 00:01:00
+#SBATCH -A edu24.DD2356
+# Number of nodes
+#SBATCH -p main
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+#SBATCH --nodes=9
+#SBATCH -e error_file.e
+
+cc -O2 -o fox fox.c
+srun -n 9 ./fox > output.txt
+
+#Clean-up:
+if [ ! -s "error_file.e" ]; then
+    rm "error_file.e"
+fi
+rm fox
+rm slurm-*.out
