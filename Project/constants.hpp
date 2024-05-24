3f628bfb1dfaba6d3cddc143b8b9fcb6d5797af4
David <david.ahnlund@gmail.com>
2024-05-24 13:25:03 +0200

    .

--- Project/constants.hpp
+++ Project/constants.hpp
@@ -1,10 +1,10 @@
 // Discritization Constants
 constexpr double h = 0.1;
-constexpr double dt= 0.04;
-constexpr size_t particles = 1000;
+constexpr double dt= 0.01;
+constexpr size_t particles = 2000;
 
 // Simulation Data
-constexpr double t_end = 4;
+constexpr double t_end = 1;
 constexpr size_t skip_frames = 1;
 
 // Physiological Constants
--- Project/run_local.sh
+++ Project/run_local.sh
@@ -1,14 +1,15 @@
 
+g++ -O2 serial.cpp -pg 
 
-g++ -O2 -pg serial.cpp -o serial
-g++ -O2 -pg standard.cpp -o standard 
+./a.out
+gprof a.out gmon.out > analysis_serial.txt
+perf stat -e instructions,cycles,L1-dcache-load-misses,L1-dcache-loads,cache-misses,duration_time ./a.out
 
-./serial
-gprof serial gmon.out > analysis_serial.txt
 
-./standard
-gprof standard gmon.out > analysis_standard.txt
+g++ -O2 standard.cpp -pg 
+./a.out
+gprof a.out gmon.out > analysis_standard.txt
+perf stat -e instructions,cycles,L1-dcache-load-misses,L1-dcache-loads,cache-misses,duration_time ./a.out
 
-rm serial
-rm standard
+rm a.out
 rm gmon.out
