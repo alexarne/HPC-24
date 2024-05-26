92b1d78245a9557159d002bb39b45de8b5acf814
David <david.ahnlund@gmail.com>
2024-05-26 16:19:43 +0200

    update

---
+++ Project/profiling.sh
@@ -0,0 +1,11 @@
+g++ -O2 -o standard standard.cpp
+g++ -O2 -o serial serial.cpp
+
+num_particles=3000
+
+sudo perf stat -o profiling_outputs/serial$num_particles.txt -e instructions,cycles,L1-dcache-load-misses,L1-dcache-loads,LLC-loads,LLC-load-misses,LLC-stores,LLC-store-misses -r 10 ./serial
+
+sudo perf stat -o profiling_outputs/standard$num_particles.txt -e instructions,cycles,L1-dcache-load-misses,L1-dcache-loads,LLC-loads,LLC-load-misses,LLC-stores,LLC-store-misses -r 10 ./standard 
+
+rm serial
+rm standard
