030c5a65fb024db0fde4f8e710b2913021e1e7ee
David Ahnlund <david.ahnlund@gmail.com>
2024-05-30 11:37:46 +0200

    updatedd profiling.sh

--- Project/profiling.sh
+++ Project/profiling.sh
@@ -5,19 +5,32 @@
 #SBATCH -A edu24.DD2356
 # Number of nodes
 #SBATCH -p main
-#SBATCH --ntasks-per-node=128
-#SBATCH --cpus-per-task=1
+#SBATCH --ntasks-per-node=4
+#SBATCH --cpus-per-task=16
 #SBATCH --nodes=1
 #SBATCH -e error_file.e
 
-
+## CRAYPAT
 module load perftools-base
 module load perftools-lite
 
-CC -O2 -o standard_mpi.x standard_mpi.cpp
+n=16
+
+export OMP_NUM_THREADS=$n
+OMP_PLACES=cores
+
+CC -O2 -o standard_mpi.x standard_mpi.cpp -openmp
 
 pat_build standard_mpi.x+pat
 
-srun ./standard_mpi.x
+srun ./standard_mpi.x > craypatoutputs/craypat_$n.txt
 
 pat_report
+
+#Clean-up:
+if [ ! -s "error_file.e" ]; then
+    rm "error_file.e"
+fi
+rm -rf standard_mpi.x+*
+rm standard_mpi.x
+rm slurm_*
